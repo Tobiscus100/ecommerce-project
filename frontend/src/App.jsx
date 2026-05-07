ae86b1b91eec1fe14c76d2281744ccf9f59c692d
Tobiscus100 <adeyinkaoluwatobiloba02@gmail.com>
2026-05-07 13:11:26 +0100

    UI: Integrated React-Bootstrap and created Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
     <Router>
-      <main>
-        <Routes>
-          <Route path='/' element={<HomeScreen />} exact />
-        </Routes>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+          </Routes>
+        </Container>
       </main>
     </Router>
   )
