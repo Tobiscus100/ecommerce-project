dc902732534083a8e4bac8c8715c3b640de707e8
Tobiscus100 <adeyinkaoluwatobiloba02@gmail.com>
2026-05-13 11:09:19 +0100

    Arch: Implemented Redux store and productList reducer

---
+++ frontend/src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { productListReducer } from './reducers/productReducers'
+
+const store = configureStore({
+  reducer: {
+    productList: productListReducer,
+  },
+})
+
+export default store
