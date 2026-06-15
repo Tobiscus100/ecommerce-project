1bc558898e2b62e7112ebb55a08447a49327ef69
Tobiscus100 <adeyinkaoluwatobiloba02@gmail.com>
2026-06-15 10:17:26 +0100

    Auth: Installed SimpleJWT and configured user login backend route

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 urlpatterns = [
+    # Auth Routes
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    
+    # Product Routes
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>/', views.getProduct, name="product"),
 ]
