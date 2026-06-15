943b9ef976df9f2fa212eb66ed431847fc808367
Tobiscus100 <adeyinkaoluwatobiloba02@gmail.com>
2026-06-15 09:35:11 +0100

    Arch: Implemented Redux Cart system with persistent localStorage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { productListReducer, productDetailsReducer} from './reducers/productReducers'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
+
+// Load cart items from local storage if they exist
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : []
 
 const store = configureStore({
-  reducer: {
-    productList: productListReducer,
-    productDetails: productDetailsReducer,
-  },
+    reducer: {
+        productList: productListReducer,
+        productDetails: productDetailsReducer,
+        cart: cartReducer, // Inject cart state
+    },
+    preloadedState: {
+        cart: { cartItems: cartItemsFromStorage }
+    }
 })
 
 export default store
