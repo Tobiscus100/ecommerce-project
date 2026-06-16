b15888ddcae1287cb7a72007825939c23ba7110e
Tobiscus100 <adeyinkaoluwatobiloba02@gmail.com>
2026-06-16 09:43:14 +0100

    Auth: Implemented Redux architecture for user login state management

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,20 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 // Load cart items from local storage if they exist
 const cartItemsFromStorage = localStorage.getItem('cartItems')
     ? JSON.parse(localStorage.getItem('cartItems'))
     : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null
+
 const store = configureStore({
     reducer: {
         productList: productListReducer,
         productDetails: productDetailsReducer,
-        cart: cartReducer, // Inject cart state
+        cart: cartReducer, 
+        userLogin: userLoginReducer,
+        // Inject cart state
     },
     preloadedState: {
-        cart: { cartItems: cartItemsFromStorage }
+        cart: { cartItems: cartItemsFromStorage },
+        userLogin: { userInfo: userInfoFromStorage }
     }
 })
 
