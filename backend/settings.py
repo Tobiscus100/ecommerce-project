ecc9657887f595601e121df7e2d34887393871ca
Tobiscus100 <adeyinkaoluwatobiloba02@gmail.com>
2026-09-16 00:56:18 +0100

    fix: allow netlify frontend domain in django cors settings

--- backend/settings.py
+++ backend/settings.py
@@ -94,12 +94,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",    
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
